48a9b9e1aa4bf94f4342f377450076aaf988d6c2
Ishlaah <muhammadsyabani5@gmail.com>
2022-04-28 01:21:55 +0700

    Tugas Praktikum PBO KE-4

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+import GUI.GUI;
+
+    public static void main(String[] args) {
+        Connector connection = new Connector();
+        GUI gui = new GUI();
+        gui.initialize();
+    }
+    
+}
